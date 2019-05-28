b954adf9b9c21c34ef4f710bcf515c8d3f1e0cf4
Liyue1998 <1757759700@qq.com>
2019-05-28 15:31:01 +0800

    second commit

--- ch07/weixinkey.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-    appid : 'wxa46c8c961bc480a5',
-    appsecret : 'eac89a2c554d95dfa135b411f7c64534'
-}
--- ch08/weixinkey.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-    //设置你的AppID和AppSecret
-    appid : '',
-    appsecret : ''
-}
